db9ad5faa157ce93900b13e1c682206112df6a47
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-21 23:37:07 +0530

    create worldwide

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'datasource.dart';
+import 'panels/worldwidepanel.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,6 +14,38 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         centerTitle: false,
         title: Text("COVID-19 TRACKER"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 100,
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              color: Colors.orange[100],
+              child: Text(
+                DataSource.quote,
+                style: TextStyle(
+                  color: Colors.orange[800],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Worldwide',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            WorldWidePanel(),
+          ],
+        ),
       ),
       
     );
---
+++ lib/panels/worldwidepanel.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class WorldWidePanel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GridView(
+        shrinkWrap: true,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 2
+        ),
+        children: <Widget>[
+          StatusPanel(),
+          StatusPanel(),
+          StatusPanel(),
+          StatusPanel(),
+        ],
+      ),
+      
+    );
+  }
+}
+
+class StatusPanel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return Container(
+      margin: EdgeInsets.all(5),
+      color: Colors.blue[100],
+      height: 80,
+      width: width/2,
+      child: Column(
+        children: <Widget>[
+          Text('CONFIRMED', style: TextStyle(fontWeight:FontWeight.bold, fontSize: 16),),
+          Text('1234', style: TextStyle(fontWeight:FontWeight.bold, fontSize: 16)),
+        ],
+      ),
+    );
+  }
+}
