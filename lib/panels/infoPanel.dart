12d729ebfa974275aec7ee36188af48135d58e54
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-22 13:56:31 +0530

    add url links

--- lib/panels/infoPanel.dart
+++ lib/panels/infoPanel.dart
@@ -1,5 +1,7 @@
 import 'package:covid19_app/datasource.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 
 class InfoPanel extends StatelessWidget {
   @override
@@ -27,43 +29,53 @@ class InfoPanel extends StatelessWidget {
             ),
           ),
 
-          Container(
-            padding: EdgeInsets.symmetric(vertical:12, horizontal:10),
-            margin: EdgeInsets.symmetric(vertical:5,horizontal:10),
-            color: primaryBlack,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  'DONATE',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
+          GestureDetector(
+            onTap: (){
+              launch('https://www.who.int/emergencies/diseases/novel-coronavirus-2019/donate');
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical:12, horizontal:10),
+              margin: EdgeInsets.symmetric(vertical:5,horizontal:10),
+              color: primaryBlack,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'DONATE',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
                   ),
-                ),
-                Icon(Icons.arrow_forward, color: Colors.white,),
-              ],
+                  Icon(Icons.arrow_forward, color: Colors.white,),
+                ],
+              ),
             ),
           ),
 
-          Container(
-            padding: EdgeInsets.symmetric(vertical:12, horizontal:10),
-            margin: EdgeInsets.symmetric(vertical:5,horizontal:10),
-            color: primaryBlack,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  'MYTH BUSTERS',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
+          GestureDetector(
+            onTap: (){
+              launch('https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public/myth-busters');
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical:12, horizontal:10),
+              margin: EdgeInsets.symmetric(vertical:5,horizontal:10),
+              color: primaryBlack,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'MYTH BUSTERS',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
                   ),
-                ),
-                Icon(Icons.arrow_forward, color: Colors.white,),
-              ],
+                  Icon(Icons.arrow_forward, color: Colors.white,),
+                ],
+              ),
             ),
           ),
         ],
