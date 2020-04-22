c6db9aa52623380fcf063b2aaad01c2f4d3d9f6f
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-22 14:37:59 +0530

    FAQs Done

---
+++ lib/pages/faqs.dart
@@ -0,0 +1,27 @@
+import 'package:covid19_app/datasource.dart';
+import 'package:flutter/material.dart';
+
+class FAQPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FAQs") ,
+      ),
+      body: ListView.builder(
+        itemCount: DataSource.questionAnswers.length,
+        itemBuilder: (context,index){
+          return ExpansionTile(
+            title: Text(DataSource.questionAnswers[index]['question'],style: TextStyle(fontWeight: FontWeight.bold),), 
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+                child: Text(DataSource.questionAnswers[index]['answer']),
+              ),
+            ], 
+          );
+        }
+      ),
+    );
+  }
+}
--- lib/panels/infoPanel.dart
+++ lib/panels/infoPanel.dart
@@ -1,4 +1,5 @@
 import 'package:covid19_app/datasource.dart';
+import 'package:covid19_app/pages/faqs.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -9,23 +10,29 @@ class InfoPanel extends StatelessWidget {
     return Container(
       child: Column(
         children: <Widget>[
-          Container(
-            padding: EdgeInsets.symmetric(vertical:12, horizontal:10),
-            margin: EdgeInsets.symmetric(vertical:5,horizontal:10),
-            color: primaryBlack,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  'FAQS',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
+          
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>FAQPage()));
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical:12, horizontal:10),
+              margin: EdgeInsets.symmetric(vertical:5,horizontal:10),
+              color: primaryBlack,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'FAQS',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
                   ),
-                ),
-                Icon(Icons.arrow_forward, color: Colors.white,),
-              ],
+                  Icon(Icons.arrow_forward, color: Colors.white,),
+                ],
+              ),
             ),
           ),
 
